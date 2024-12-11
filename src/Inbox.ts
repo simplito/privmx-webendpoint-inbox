6514e34d2984100a8b7a19c0d0322100a2b01ab3
Kamil Żurański <kamil@privmx.com>
2024-12-11 10:30:24 +0100

    fix: make connectPublic() same as in native Endpoint impl

--- src/Inbox.ts
+++ src/Inbox.ts
@@ -1,13 +1,12 @@
 import { ConnectionManager } from "./ConnectionManager";
 import { CryptoService } from "./crypto/Crypto";
-import * as Ecc from "./crypto/ecc";
 import { InboxApi } from "./InboxApi";
 
 export class Inbox {
     constructor(private connectionManager: ConnectionManager) {}
     
-    async connectPublic(bridgeUrl: string, solutionId: string, userPrivKeyWIF?: string): Promise<InboxApi> {
-        const privKey = userPrivKeyWIF ? await Ecc.PrivateKey.fromWIF(userPrivKeyWIF) : CryptoService.eccPrivRandom();
+    async connectPublic(solutionId: string, bridgeUrl: string): Promise<InboxApi> {
+        const privKey = CryptoService.eccPrivRandom();
         const connection = await this.connectionManager.createEcdheConnection(
             {key: privKey, solution: solutionId},
             {url: bridgeUrl, host: "_", websocket: false}
